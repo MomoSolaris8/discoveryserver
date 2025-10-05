37f6c1ef4807c0b965d39c26241d99eeb80de3c1
momo <momosunshine5@gmail.com>
2025-10-05 12:41:25 +0200

    fix: modify SQL query to correctly handle 90-day credential expiration logic

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # discoveryserver
+# discoveryserver
--- src/main/java/com/momoio/discoveryserver/Application.java
+++ src/main/java/com/momoio/discoveryserver/Application.java
@@ -1,10 +1,12 @@
 package com.momoio.discoveryserver;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
@@ -14,4 +16,13 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+//    @Bean
+//	public CommandLineRunner startup(BCryptPasswordEncoder encoder) {
+//		return args -> {
+//			var password = encoder.encode("letMeinMomo");
+//			System.out.println(password); //$2a$12$SJu0iYfBaZeSk91hGj9m3OBbTDG5FPbNk36kVaG7MuzpDjoub0T2S
+//		};
+//
+//	}
 }
